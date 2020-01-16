687904b06d747f77a432a001544a300ae71974b0
mferrarini <maura.ferrarini@gmail.com>
2020-01-16 11:23:30 -0500

    code updates 1.16.19

---
+++ childtheme.js
@@ -0,0 +1,17 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+
+<script>
+$(document).ready(function(){
+
+// Add placeholder text in searchbox
+$("input").attr("placeholder", "Search this site");
+
+// Change page header from 'Search' to 'Saerch Results'
+$("#page-title:contains(Search)").text("Search Results");
+
+// Add 'Admin Login' after the editable content in the footer
+
+// If no href in breadcrumb link, remove pointer and hover and change color to #6c6c6c
+
+});
+</script>
